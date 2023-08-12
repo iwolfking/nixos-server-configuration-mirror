b4d90becb856cc04489461917a080b43f0579b78
Kyle Robertson <me@iwolfking.xyz>
2023-08-12 00:42:56 -0500

    Fix lidarr being unable to access files

--- compose/arrstack/compose/lidarr/arion-compose.nix
+++ compose/arrstack/compose/lidarr/arion-compose.nix
@@ -4,7 +4,7 @@
     service.volumes = [
       "/mnt/server_data/data/lidarr:/config"
       "/mnt/server_data/downloads:/downloads"
-      "/mnt/server_data/media/music:/music"
+      "/mnt/server_data/media/NewMusic:/music"
     ];
     service.ports = ["9023:8686"];
     service.restart = "unless-stopped";
