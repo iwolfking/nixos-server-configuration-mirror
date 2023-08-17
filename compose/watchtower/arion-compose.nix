bd271fd40cfca62c71623cf97771908901aeb5c8
iwolfking <me@iwolfking.xyz>
2023-08-16 20:42:10 -0500

    Fiux watchtower image in compose

--- compose/watchtower/arion-compose.nix
+++ compose/watchtower/arion-compose.nix
@@ -1,6 +1,6 @@
 {
   services.watchtower = {
-    service.image = "containrrr:watchtower";
+    service.image = "containrrr/watchtower";
     service.volumes = ["/var/run/docker.sock:/var/run/docker.sock"];
     service.restart = "unless-stopped";
     service.environment.TZ = "America/Chicago";
