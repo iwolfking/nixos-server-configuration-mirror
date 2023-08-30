4ec98b0ae6079745a84131019cb6fccbc8f49910
Kyle Robertson <me@iwolfking.xyz>
2023-08-30 13:56:36 -0500

    Edit arion-compose.nix

--- compose/nextcloud/compose/nextcloud/arion-compose.nix
+++ compose/nextcloud/compose/nextcloud/arion-compose.nix
@@ -9,6 +9,15 @@
     service.environment.POSTGRES_HOST = "db";
     service.env_file = [nextcloudEnvironment];
   };
+  
+  services.app = {
+    service.image = "nextcloud:latest";
+    service.volumes = ["/mnt/server_data/data/nextcloud/data:/var/www/html"];
+    service.restart = "unless-stopped";
+    service.environment.TZ = "America/Chicago";
+    service.env_file = [nextcloudEnvironment];
+	entrypoint: /cron.sh
+  };
 
   services.db = {
     service.image = "postgres:13.4";
